115b511f3494c2233669e5b53928a88de15d92ca
JIHYE <jihyem75@gmail.com>
2023-02-24 11:11:56 +0900

    :sparkles: feat: Search, Add method

--- main.go
+++ main.go
@@ -14,4 +14,20 @@ func main() {
 	fmt.Println(account)
 
 	dictionary := mydict.Dictionary{}
+
+	word := "hello"
+	definition := "Greeting"
+
+	err := dictionary.Add(word, definition)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found:", word, "definition:", hello)
+
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
 }
