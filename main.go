8489fe71cc74f8e009a300c02271ce311a299a2b
JIHYE <jihyem75@gmail.com>
2023-02-28 12:30:40 +0900

    :sparkles: extractJab -1

--- main.go
+++ main.go
@@ -9,24 +9,30 @@ import (
 	"strconv"
 )
 
-var baseURL = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	summary  string
+}
+
+var baseURL = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=java"
 
 func main() {
 	totalPages := getPages()
 
-	for i := 0; i < totalPages; i++ {
+	for i := 1; i <= totalPages; i++ {
 		getPage(i)
 	}
 }
 
 func getPage(page int) {
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page) + "&recruitSort=relation&recruitPageCount=40" +
+		"&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10" +
+		"&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n"
 	fmt.Println("Requesting", pageURL)
-}
 
-func getPages() int {
-	pages := 0
-	res, err := http.Get(baseURL)
+	res, err := http.Get(pageURL)
 
 	checkErr(err)
 	checkCode(res)
@@ -41,10 +47,36 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("value")
+		title := card.Find(".job_tit>a").Text()
+		location := card.Find(".job_condition>span>a").Text()
+
+		fmt.Println(id, title, location)
+	})
+}
+
+func cleanString(str string) string {
+	return ""
+}
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
 	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
 		pages = s.Find("a").Length()
 	})
-
 	return pages
 }
 
