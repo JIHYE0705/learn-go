5fccd156d6608baace114eda79e47ad0cb033c8b
JIHYE <jihyem75@gmail.com>
2023-02-28 14:06:39 +0900

    :sparkles: extractJab -2

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 type extractedJob struct {
@@ -19,14 +20,19 @@ type extractedJob struct {
 var baseURL = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=java"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 1; i <= totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page) + "&recruitSort=relation&recruitPageCount=40" +
 		"&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10" +
 		"&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n"
@@ -50,16 +56,29 @@ func getPage(page int) {
 	searchCards := doc.Find(".item_recruit")
 
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		id, _ := card.Attr("value")
-		title := card.Find(".job_tit>a").Text()
-		location := card.Find(".job_condition>span>a").Text()
-
-		fmt.Println(id, title, location)
+		job := extractJob(card)
+		jobs = append(jobs, job)
 	})
+
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("value")
+	title := cleanString(card.Find(".job_tit>a").Text())
+	location := cleanString(card.Find(".job_condition>span>a").Text())
+	summary := cleanString(card.Find(".job_sector>b>a").Text()) + cleanString(card.Find(".job_sector>a").Text())
+
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		summary:  summary}
+
 }
 
 func cleanString(str string) string {
-	return ""
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
@@ -69,7 +88,12 @@ func getPages() int {
 	checkErr(err)
 	checkCode(res)
 
-	defer res.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(res.Body)
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
