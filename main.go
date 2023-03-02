1aff35733c214ded8cc74843a1588d3b085c5df2
JIHYE <jihyem75@gmail.com>
2023-03-02 10:14:34 +0900

    :sparkles: feat: file download

--- main.go
+++ main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"github.com/JIHYE0705/learngo/scrapper"
 	"github.com/labstack/echo"
+	"log"
+	"os"
 	"strings"
 )
+
+const FileName string = "jobs.csv"
 
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
+	defer func() {
+		err := os.Remove(FileName)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
 	scrapper.Scrape(term)
-	return c.Attachment("jobs.csv", "job.csv")
+
+	return c.Attachment(FileName, FileName)
 }
 
 func main() {
